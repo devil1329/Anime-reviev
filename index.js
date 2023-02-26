d299c36b7c317266b4839dce69f48d6d47e586d5
vishnu  kumar singh <68421295+devil1329@users.noreply.github.com>
2023-02-27 01:43:38 +0530

    Alternate (#9)
    * day-2
    
    * introduce first card section
    
    * minor change
    
    * minor changes
    
    * minor change
    
    * contact us introduced
    
    * updated 22-02-2023
    
    * footer initiated
    
    * Beginner block created
    
    * changed:
    
    * Hero completed
    
    * updated
    
    * new section introduced
    
    * updated
    
    * new section added
    
    * aboutUs created
    
    * aboutUs updated
    
    * aboutUs updated
    
    ---------
    
    Co-authored-by: suvi2913 <server.visu143@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var heroShinker = function() {
+var heroShinker = function () {
     var hero = $('.hero-nav'),
         heroHeight = $('.hero-nav').outerHeight(true);
     var navBar = $('#NavBar');
     var carousel = $('#heroCarousel');
-        $(hero).parent().css('padding-top', heroHeight);
-    $(window).scroll(function() {
+    $(hero).parent().css('padding-top', heroHeight);
+    $(window).scroll(function () {
         var scrollOffset = $(window).scrollTop();
         if (scrollOffset < heroHeight) {
             $(hero).css('height', (heroHeight - scrollOffset));
@@ -13,7 +13,7 @@ var heroShinker = function() {
             hero.addClass('fixme');
             navBar.addClass('navBgColor');
             carousel.fadeOut("fast");
-    } else {
+        } else {
             hero.removeClass('fixme');
             carousel.fadeIn("fast");
             navBar.removeClass('navBgColor');
@@ -21,3 +21,56 @@ var heroShinker = function() {
     });
 }
 heroShinker();
+
+// about us 
+$(document).ready(function () {
+
+    var curPage = 1;
+    var numOfPages = $(".skw-page").length;
+    var animTime = 1000;
+    var scrolling = false;
+    var pgPrefix = ".skw-page-";
+
+    function pagination() {
+        scrolling = true;
+
+        $(pgPrefix + curPage).removeClass("inactive").addClass("active");
+        $(pgPrefix + (curPage - 1)).addClass("inactive");
+        $(pgPrefix + (curPage + 1)).removeClass("active");
+
+        setTimeout(function () {
+            scrolling = false;
+        }, animTime);
+    };
+
+    function navigateUp() {
+        if (curPage === 1) return;
+        curPage--;
+        pagination();
+    };
+
+    function navigateDown() {
+        if (curPage === numOfPages) return;
+        curPage++;
+        pagination();
+    };
+
+    $(document).on("mousewheel DOMMouseScroll", function (e) {
+        if (scrolling) return;
+        if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+            navigateUp();
+        } else {
+            navigateDown();
+        }
+    });
+
+    $(document).on("keydown", function (e) {
+        if (scrolling) return;
+        if (e.which === 38) {
+            navigateUp();
+        } else if (e.which === 40) {
+            navigateDown();
+        }
+    });
+
+});
